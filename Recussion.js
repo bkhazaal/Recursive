4b15f2a5b5a3bb1344295aae667b4d2fc9307835
benkhazaal <thajukez101@gmail.com>
2024-03-01 16:41:51 -0700

    recursionshit

---
+++ Recussion.js
@@ -0,0 +1,11 @@
+function recursion (n) {
+    if (n === 0 || n === 1) {
+        return 1;
+
+    } else {
+        return n * recursion(n - 1);
+    }
+}
+
+const num = 5;
+console.log(recursion(num));
